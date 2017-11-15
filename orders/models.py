217d5b9eb53c2dc86e5de0a773431c1caf840cfe
GiaEla <lavricevaspela@gmail.com>
2017-11-15 09:53:29 +0100

    Basic first page created.

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField('Ime izdelka', max_length=40)
+    img = models.ImageField('Fotografija')
+    size = models.CharField('Velikost', max_length=20)
+    price = models.DecimalField('Cena v €',  max_digits=8, decimal_places=2)
+    sold = models.BooleanField('Prodano', default=False)
+
+    class Meta:
+        verbose_name = 'Izdelek'
+        verbose_name_plural = 'Izdelki'
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from orders.models import Product
+
+
+def index(request):
+    last_products = Product.objects.all().order_by('-id')[:3]
+    all_products = Product.objects.all()
+    return render(request, 'index.html', {'last_products': last_products, 'all_products': all_products})
